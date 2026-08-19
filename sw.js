c5df467fda65a8d8458c69eef8cd05a76cd5ff5f
Ame0thyst <kokoo15011209@gmail.com>
2026-08-19 18:57:59 +0700

    Add animated cute preloader opening screen for Ikoo & Erika

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deep-truths-v11';
+const CACHE_NAME = 'deep-truths-v12';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -42,6 +42,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
