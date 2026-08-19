ba08ffe90c99a0cfc1b185c136e8293ffcdf7126
Ame0thyst <kokoo15011209@gmail.com>
2026-08-19 18:15:37 +0700

    Deploy new version with verified simple fetch rules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deep-truths-v6';
+const CACHE_NAME = 'deep-truths-v7';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -42,6 +42,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
